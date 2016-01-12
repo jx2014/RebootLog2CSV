c742a7c128840706509ca2b142fa12ae774f9514
JX_ULTRA <jx@xj.com>
2016-01-12 10:27:23 -0800

    1st commit
    Signed-off-by: JX_ULTRA <jx@xj.com>

---
+++ RebootLog2CSV/main.py
@@ -0,0 +1,16 @@
+#
+#Program name: RebootLog2CSV
+#
+#Description: extract mac IDs from net mgr log file, 
+#             and extract reboot counter for each mac IDs,
+#             output to CSV file
+#
+#Author:      JX
+#Date:        2016-1-12
+#
+#
+
+print "hello"
+
+
+
